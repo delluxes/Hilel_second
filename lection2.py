7b2d19214af52f2887ff27572a1002ad2efefedf
Delluxe <qulle132@gmail.com>
2022-07-27 02:04:11 +0300

    lection commit

---
+++ lection2.py
@@ -0,0 +1,30 @@
+#data types
+result1 = 1 + 1
+print(result1)
+#TO DO comment important
+print(type(result)) #type of obect
+
+a = 5
+b = a
+print(a, b)
+
+#multiple
+c, d = 5, 6
+
+value_str = "hello"
+value_int = 2
+result = value_int + value_str
+print(result)
+
+texti: str = "text"
+
+
+# \n perenos
+# \t tabulation
+
+
+format_str = f'put info - {texti}'
+
+name = input('your name')
+display_name = f'Hello {name}'
+print(display_name)
