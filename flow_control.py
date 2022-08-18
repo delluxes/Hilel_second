857e8bcdcfcb5d0c49b81ce127221ed73ffa4905
Delluxe <qulle132@gmail.com>
2022-08-18 22:58:18 +0300

    Calculator

---
+++ flow_control.py
@@ -0,0 +1,26 @@
+while True:
+    first_num = (input('First symbol:'))
+    if first_num == 'exit':
+        print('Closed')
+        break
+    first_num = float(first_num) if first_num.find(".") != -1 else int(first_num)
+    second_num = (input('Second symbol:'))
+    if second_num == 'exit':
+        print('Closed')
+        break
+    second_num = float(second_num) if second_num.find(".") != -1 else int(second_num)
+    operation = input('Choose your item(+,-,/,*):')
+
+    if operation not in ('+', '-', '/', '*'):
+        print('Only Symbols')
+        continue
+    if operation == '+':
+        print(first_num + second_num)
+    elif operation == '-':
+        print(first_num - second_num)
+    elif operation == '/':
+        print(first_num / second_num)
+    elif operation == '*':
+        print(first_num * second_num)
+
+
--- rows_and_numbers_advance.py
+++ rows_and_numbers_advance.py
@@ -2,5 +2,5 @@ first_name = input('Write your name here:')
 name = first_name.strip()
 
 print('Hello, dear:', name.capitalize())
-print(f'Your name has:{len(name)} symbols' )
+print(f'Your name has:{len(name)} symbols')
 print(name.capitalize()[::-1])
